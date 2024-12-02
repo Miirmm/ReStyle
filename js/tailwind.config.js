f6b04c41c4fdd25796589cf753d7dbbccaa3bf17
Miirmm <lefebvre.mael.76@gmail.com>
2024-12-02 14:52:27 +0100

    0

---
+++ js/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        colors: {
+            'text': '#001008',
+            'background': '#edfff7',
+            'primary': '#0bfd8c',
+            'secondary': '#8088fe',
+            'accent': '#8a41fe',
+        }
+        }
+    }
